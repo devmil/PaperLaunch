d20cebb35a423d3f88d6f7e0c27601a3cdd696e4
Devmil (Michael Lamers) <devmil@posteo.de>
2019-03-24 20:21:04 +0100

    build.sh renames local.properties in case it is present

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
-set -xeuo pipefail
-gradle test
-gradle assembleDebug
-gradle assembleRelease
-chown -R `stat -c "%u:%g" $(pwd)/app` $(pwd)/app/build
-chown -R `stat -c "%u:%g" $(pwd)/app` $(pwd)/build
+#set -xeuo pipefail
+set -x pipefail
+
+# rename local properties if they are present (e.g. when the docker build is started locally)
+if [ -f ./local.properties ]; then
+    mv ./local.properties ./local.properties.tmp
+fi
+
+# build scope
+(
+    set -e
+    gradle test
+    gradle assembleDebug
+    gradle assembleRelease
+    chown -R `stat -c "%u:%g" $(pwd)/app` $(pwd)/app/build
+    chown -R `stat -c "%u:%g" $(pwd)/app` $(pwd)/build
+)
+
+cmdresult=$?
+
+# rename the local properties back after the build is finished
+if [ -f ./local.properties.tmp ]; then
+    mv ./local.properties.tmp ./local.properties
+fi
+
+# return the build result
+exit $cmdresult
--- build_docker.ps1
+++ build_docker.ps1
@@ -6,4 +6,4 @@ $path = $path -replace ':', ''
 $path = $path -replace '\\', '/'
 $path = $path -replace 'c/Users', '/c/Users'
 
-docker.exe run --rm -it -v ${path}:/home/gradle/ -w /home/gradle paperlaunch-build /bin/bash -c "/usr/bin/dos2unix -n .ci/build.sh .ci/build_lf.sh && bash .ci/build_lf.sh && rm .ci/build_lf.sh"
+docker.exe run --rm -it -v ${path}:/home/gradle/ -w /home/gradle paperlaunch-build /bin/bash -c "bash_r .ci/build.sh"
