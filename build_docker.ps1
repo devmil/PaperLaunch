7c66725fe12d693d50a49964354e80ce7d0116d8
Devmil (Michael Lamers) <devmil@posteo.de>
2019-08-16 22:17:34 +0200

    Docker adaptions for WSL / WSL2

--- build_docker.ps1
+++ build_docker.ps1
@@ -1,9 +1,3 @@
 docker.exe build -t paperlaunch-build .
 
-# for Docker Toolbox (not sure if Docker would need this)
-$path = $PSScriptRoot
-$path = $path -replace ':', ''
-$path = $path -replace '\\', '/'
-$path = $path -replace 'c/Users', '/c/Users'
-
-docker.exe run --rm -it -v ${path}:/home/gradle/ -w /home/gradle paperlaunch-build /bin/bash -c "bash_r .ci/build.sh"
+docker.exe run --rm -it -v ${PSScriptRoot}:/home/gradle/ -w /home/gradle paperlaunch-build /bin/bash -c "bash_r .ci/build.sh"
